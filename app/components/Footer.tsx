664120f5c2422b358f8df04ef5f56c7f2ede68fa
aashish <shahaashih@gmail.com>
2025-07-06 19:32:15 +0545

    completed footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,15 +2,22 @@ import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div id='footer' className="bg-secondary-background h-[200px] ">
-    <div className="container flex flex-col justify-center items-center">
-      <div className="flex gap-6 items-center">
-        <Link href="#">  <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="#ebe2e2" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"/></svg></Link>
+  return <div id='footer' className="bg-secondary-background min-h-[200px] h-fit py-10">
+    <div className="container flex flex-col justify-center items-center h-full gap-4">
+      <h1 className="text-red-400 font-semibold text-xl text-center font-dmSans">Follow me for more information!</h1>
+      <div className="flex gap-4 items-center ">
+        <Link href="https://www.facebook.com/aashishbabu.shah">  <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="#ebe2e2" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95"/></svg></Link>
         <Link href="#"></Link>
-    
+  <Link href="https://www.instagram.com/aashishbabushah/"><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="#ebe2e2" d="M12 8.75a3.25 3.25 0 1 0 0 6.5a3.25 3.25 0 0 0 0-6.5"/><path fill="#ebe2e2" fillRule="evenodd" d="M6.77 3.082a47.5 47.5 0 0 1 10.46 0c1.899.212 3.43 1.707 3.653 3.613a45.7 45.7 0 0 1 0 10.61c-.223 1.906-1.754 3.401-3.652 3.614a47.5 47.5 0 0 1-10.461 0c-1.899-.213-3.43-1.708-3.653-3.613a45.7 45.7 0 0 1 0-10.611C3.34 4.789 4.871 3.294 6.77 3.082M17 6a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-9.75 6a4.75 4.75 0 1 1 9.5 0a4.75 4.75 0 0 1-9.5 0" clipRule="evenodd"/></svg></Link>
 
+  <Link href="https://github.com/Aashish9840"><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="#ebe2e2" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/></svg></Link>
+  <Link href='https://www.linkedin.com/in/aashishbabu-shah-3681a7216/'><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="#ebe2e2" d="M20.47 2H3.53a1.45 1.45 0 0 0-1.47 1.43v17.14A1.45 1.45 0 0 0 3.53 22h16.94a1.45 1.45 0 0 0 1.47-1.43V3.43A1.45 1.45 0 0 0 20.47 2M8.09 18.74h-3v-9h3ZM6.59 8.48a1.56 1.56 0 1 1 0-3.12a1.57 1.57 0 1 1 0 3.12m12.32 10.26h-3v-4.83c0-1.21-.43-2-1.52-2A1.65 1.65 0 0 0 12.85 13a2 2 0 0 0-.1.73v5h-3v-9h3V11a3 3 0 0 1 2.71-1.5c2 0 3.45 1.29 3.45 4.06Z"/></svg></Link>
       </div>
-      
+      <div className="border-t border-t-gray-100 w-full py-8 flex justify-between md:px-10 ">
+        <h1 className="text-sm text-center font-medium font-dmSans text-white cursor-pointer ">@2025 Copyright! <span>Portfolio</span></h1>
+        <h1  className="text-sm text-center font-medium font-dmSans text-white cursor-pointer ">Terms & Conditions</h1>
+                <h1  className="text-sm text-center font-medium font-dmSans text-white cursor-pointer ">Supports</h1>
+      </div>
 
     </div>
 
